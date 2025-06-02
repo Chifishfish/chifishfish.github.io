a019961fecc3431fd416555bda3e5dc8c66a5d71
Louis Lycanl <127604262+Chifishfish@users.noreply.github.com>
2025-06-02 15:13:55 +0800

    Add files via upload

--- assets/js/Eject-news.js
+++ assets/js/Eject-news.js
@@ -1,4 +1,4 @@
 // 弹出欢迎提示框，显示欢迎信息
-window.alert("Hello！\n\nWelcome to Chifishfish's Website！\n\n如果你在使用过程中遇到问题，务必联系我！！\n\nPlease contact me if you have any troubles about the site!");
+window.alert("Hello！\n\nWelcome to Chifishfish's Website！\n\n如果你在使用过程中遇到问题，务必联系我！！\n\nIf you encounter any problems during use, please contact me immediately!");
 // 在控制台输出提示框显示的日志信息
 console.log("Alert displayed to the user.");
---
+++ assets/js/halo.js
@@ -0,0 +1,14 @@
+VANTA.HALO({
+    el: "#vanta-bg",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    baseColor: 0xff2e00,
+    backgroundColor: 0x0,
+    amplitudeFactor: 1.99,
+    xOffset: -0.08,
+    yOffset: -0.05,
+    size: 3.10
+})
